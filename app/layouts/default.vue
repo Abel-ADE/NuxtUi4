b11335df850442e50171b887594ee4704acfca15
Abel <abeliglesiasmoure@gmail.com>
2025-09-24 17:01:08 +0200

    Speed Insigths

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,18 +1,21 @@
 <template>
-    <UApp>
-        <Header />
-        <UMain>
-            <slot />
-            <UButton 
-            icon="i-lucide-arrow-up" 
-            class="fixed right-4 bottom-4"
-            onclick="window.scrollTo(0, 0)"/>
-        </UMain>
-        <Footer />
-    </UApp>
+  <UApp>
+    <Header />
+    <UMain>
+      <slot />
+      <UButton
+        icon="i-lucide-arrow-up"
+        class="fixed right-4 bottom-4"
+        onclick="window.scrollTo(0, 0)"
+      />
+      <SpeedInsights />
+    </UMain>
+    <Footer />
+  </UApp>
 </template>
 
 <script setup lang="ts">
-import Footer from '~/components/layouts/footer.vue';
-import Header from '~/components/layouts/header.vue';
+import Footer from "~/components/layouts/footer.vue";
+import Header from "~/components/layouts/header.vue";
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
 </script>
